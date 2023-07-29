fb7155ba4829de5cb83853968728bee65b219161
jahirxtrap <jahirroman2002@gmail.com>
2023-07-28 23:35:53 -0500

    Improve environment

--- src/main/java/com/jahirtrap/ingotcraft/IngotCraftMod.java
+++ src/main/java/com/jahirtrap/ingotcraft/IngotCraftMod.java
@@ -3,41 +3,20 @@ package com.jahirtrap.ingotcraft;
 import com.jahirtrap.ingotcraft.init.IngotcraftModBlocks;
 import com.jahirtrap.ingotcraft.init.IngotcraftModItems;
 import com.jahirtrap.ingotcraft.init.IngotcraftModTabs;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
-
-@Mod("ingotcraft")
+@Mod(IngotcraftMod.MODID)
 public class IngotcraftMod {
-    public static final Logger LOGGER = LogManager.getLogger(IngotcraftMod.class);
+
     public static final String MODID = "ingotcraft";
-    private static final String PROTOCOL_VERSION = "1";
-    public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(new ResourceLocation(MODID, MODID), () -> PROTOCOL_VERSION,
-            PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
-    private static int messageID = 0;
 
     public IngotcraftMod() {
         IngotcraftModTabs.load();
+
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         IngotcraftModBlocks.REGISTRY.register(bus);
         IngotcraftModItems.REGISTRY.register(bus);
-
-    }
-
-    public static <T> void addNetworkMessage(Class<T> messageType, BiConsumer<T, FriendlyByteBuf> encoder, Function<FriendlyByteBuf, T> decoder,
-                                             BiConsumer<T, Supplier<NetworkEvent.Context>> messageConsumer) {
-        PACKET_HANDLER.registerMessage(messageID, messageType, encoder, decoder, messageConsumer);
-        messageID++;
     }
 }
