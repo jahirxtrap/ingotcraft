c44f9e699bebf3217701beb2e504d99c923757c7
jahirxtrap <jahirroman2002@gmail.com>
2024-03-17 07:13:35 -0500

    Bus

--- neoforge/src/main/java/com/jahirtrap/ingotcraft/IngotcraftMod.java
+++ neoforge/src/main/java/com/jahirtrap/ingotcraft/IngotcraftMod.java
@@ -4,16 +4,13 @@ import com.jahirtrap.ingotcraft.init.IngotcraftModBlocks;
 import com.jahirtrap.ingotcraft.init.IngotcraftModItems;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(IngotcraftMod.MODID)
 public class IngotcraftMod {
 
     public static final String MODID = "ingotcraft";
 
-    public IngotcraftMod() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
+    public IngotcraftMod(IEventBus bus) {
         IngotcraftModBlocks.REGISTRY.register(bus);
         IngotcraftModItems.REGISTRY.register(bus);
         IngotcraftModTab.init();
