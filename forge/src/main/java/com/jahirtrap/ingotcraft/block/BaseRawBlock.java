517f5c5f71dcfe4106102dcd4a2110011f248d8e
jahirxtrap <jahirroman2002@gmail.com>
2024-06-26 22:07:23 -0500

    Update

---
+++ forge/src/main/java/com/jahirtrap/ingotcraft/block/BaseRawBlock.java
@@ -0,0 +1,10 @@
+package com.jahirtrap.ingotcraft.block;
+
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.MapColor;
+
+public class BaseRawBlock extends BaseBlock {
+    public BaseRawBlock() {
+        super(MapColor.STONE, SoundType.STONE);
+    }
+}
--- neoforge/src/main/java/com/jahirtrap/ingotcraft/init/IngotcraftModBlocks.java
+++ neoforge/src/main/java/com/jahirtrap/ingotcraft/init/IngotcraftModBlocks.java
@@ -1,21 +1,25 @@
 package com.jahirtrap.ingotcraft.init;
 
-import com.jahirtrap.ingotcraft.IngotcraftMod;
-import com.jahirtrap.ingotcraft.block.*;
+import com.jahirtrap.ingotcraft.block.BaseBlock;
+import com.jahirtrap.ingotcraft.block.BaseRawBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.MapColor;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import static com.jahirtrap.ingotcraft.IngotcraftMod.MODID;
+
 public class IngotcraftModBlocks {
-    public static final DeferredRegister.Blocks REGISTRY = DeferredRegister.createBlocks(IngotcraftMod.MODID);
-    public static final DeferredBlock<Block> RAW_STEEL_BLOCK = REGISTRY.register("raw_steel_block", RawSteelBlock::new);
-    public static final DeferredBlock<Block> RAW_BRONZE_BLOCK = REGISTRY.register("raw_bronze_block", RawBronzeBlock::new);
-    public static final DeferredBlock<Block> RAW_LEAD_BLOCK = REGISTRY.register("raw_lead_block", RawLeadBlock::new);
-    public static final DeferredBlock<Block> RAW_SILVER_BLOCK = REGISTRY.register("raw_silver_block", RawSilverBlock::new);
-    public static final DeferredBlock<Block> RAW_TIN_BLOCK = REGISTRY.register("raw_tin_block", RawTinBlock::new);
-    public static final DeferredBlock<Block> STEEL_BLOCK = REGISTRY.register("steel_block", SteelBlock::new);
-    public static final DeferredBlock<Block> BRONZE_BLOCK = REGISTRY.register("bronze_block", BronzeBlock::new);
-    public static final DeferredBlock<Block> LEAD_BLOCK = REGISTRY.register("lead_block", LeadBlock::new);
-    public static final DeferredBlock<Block> SILVER_BLOCK = REGISTRY.register("silver_block", SilverBlock::new);
-    public static final DeferredBlock<Block> TIN_BLOCK = REGISTRY.register("tin_block", TinBlock::new);
+    public static final DeferredRegister.Blocks REGISTRY = DeferredRegister.createBlocks(MODID);
+    public static final DeferredBlock<Block> RAW_STEEL_BLOCK = REGISTRY.register("raw_steel_block", BaseRawBlock::new);
+    public static final DeferredBlock<Block> RAW_BRONZE_BLOCK = REGISTRY.register("raw_bronze_block", BaseRawBlock::new);
+    public static final DeferredBlock<Block> RAW_LEAD_BLOCK = REGISTRY.register("raw_lead_block", BaseRawBlock::new);
+    public static final DeferredBlock<Block> RAW_SILVER_BLOCK = REGISTRY.register("raw_silver_block", BaseRawBlock::new);
+    public static final DeferredBlock<Block> RAW_TIN_BLOCK = REGISTRY.register("raw_tin_block", BaseRawBlock::new);
+    public static final DeferredBlock<Block> STEEL_BLOCK = REGISTRY.register("steel_block", () -> new BaseBlock(MapColor.METAL, SoundType.METAL));
+    public static final DeferredBlock<Block> BRONZE_BLOCK = REGISTRY.register("bronze_block", () -> new BaseBlock(MapColor.METAL, SoundType.METAL));
+    public static final DeferredBlock<Block> LEAD_BLOCK = REGISTRY.register("lead_block", () -> new BaseBlock(MapColor.METAL, SoundType.COPPER));
+    public static final DeferredBlock<Block> SILVER_BLOCK = REGISTRY.register("silver_block", () -> new BaseBlock(MapColor.METAL, SoundType.METAL));
+    public static final DeferredBlock<Block> TIN_BLOCK = REGISTRY.register("tin_block", () -> new BaseBlock(MapColor.METAL, SoundType.METAL));
 }
