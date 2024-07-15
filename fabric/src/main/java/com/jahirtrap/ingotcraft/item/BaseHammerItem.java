c94f25b8736343d32ae43b5166b26b9fb7bb9775
jahirxtrap <jahirroman2002@gmail.com>
2024-07-15 06:24:52 -0500

    Fixes

--- fabric/src/main/java/com/jahirtrap/ingotcraft/item/BaseHammerItem.java
+++ fabric/src/main/java/com/jahirtrap/ingotcraft/item/BaseHammerItem.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tier;
 
 import static com.jahirtrap.ingotcraft.IngotcraftModTab.TAB_INGOTCRAFT;
+import static com.jahirtrap.ingotcraft.util.CommonUtils.hurt;
 
 public class BaseHammerItem extends PickaxeItem implements RepairableItem, FabricItem {
     public BaseHammerItem(Tier tier, Properties properties) {
@@ -20,13 +21,7 @@ public class BaseHammerItem extends PickaxeItem implements RepairableItem, Fabri
 
     @Override
     public ItemStack getRecipeRemainder(ItemStack stack) {
-        ItemStack retVal = stack.copy();
-        if (retVal.getTag() != null && retVal.getTag().getBoolean("Unbreakable")) return retVal;
-        retVal.setDamageValue(stack.getDamageValue() + 1);
-        if (retVal.getDamageValue() >= retVal.getMaxDamage()) {
-            return ItemStack.EMPTY;
-        }
-        return retVal;
+        return hurt(1, stack.copy());
     }
 
     @Override
