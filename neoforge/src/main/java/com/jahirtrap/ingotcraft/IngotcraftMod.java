cc9958a6d5afb1ca7bcf80feb0d76fa67d959405
jahirxtrap <jahirroman2002@gmail.com>
2024-07-30 19:54:50 -0500

    Update environment

--- neoforge/src/main/java/com/jahirtrap/ingotcraft/IngotcraftMod.java
+++ neoforge/src/main/java/com/jahirtrap/ingotcraft/IngotcraftMod.java
@@ -1,6 +1,6 @@
 package com.jahirtrap.ingotcraft;
 
-import com.jahirtrap.ingotcraft.init.ModItems;
+import com.jahirtrap.ingotcraft.init.ModContent;
 import com.jahirtrap.ingotcraft.init.ModTab;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -11,7 +11,7 @@ public class IngotcraftMod {
     public static final String MODID = "ingotcraft";
 
     public IngotcraftMod(IEventBus bus) {
-        ModItems.init(bus);
+        ModContent.init(bus);
         ModTab.init(bus);
     }
 }
