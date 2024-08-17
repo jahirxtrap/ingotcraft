6078fccc7fb7edf936f44b6bd0c0f7b36e777775
jahirxtrap <jahirroman2002@gmail.com>
2024-08-17 01:34:56 -0500

    Registry

--- fabric/src/main/java/com/jahirtrap/ingotcraft/init/ModTab.java
+++ fabric/src/main/java/com/jahirtrap/ingotcraft/init/ModTab.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 import static com.jahirtrap.ingotcraft.IngotcraftMod.MODID;
@@ -13,58 +14,8 @@ public class ModTab {
         CreativeModeTab TAB_INGOTCRAFT = FabricItemGroup.builder(new ResourceLocation(MODID, "tab_ingotcraft"))
                 .icon(() -> new ItemStack(ModContent.NETHERITE_PICK_HAMMER))
                 .displayItems((features, event) -> {
-                    event.accept(ModContent.RAW_STEEL);
-                    event.accept(ModContent.RAW_BRONZE);
-                    event.accept(ModContent.RAW_LEAD);
-                    event.accept(ModContent.RAW_SILVER);
-                    event.accept(ModContent.RAW_TIN);
-                    event.accept(ModContent.RAW_STEEL_BLOCK);
-                    event.accept(ModContent.RAW_BRONZE_BLOCK);
-                    event.accept(ModContent.RAW_LEAD_BLOCK);
-                    event.accept(ModContent.RAW_SILVER_BLOCK);
-                    event.accept(ModContent.RAW_TIN_BLOCK);
-                    event.accept(ModContent.STEEL_INGOT);
-                    event.accept(ModContent.BRONZE_INGOT);
-                    event.accept(ModContent.LEAD_INGOT);
-                    event.accept(ModContent.SILVER_INGOT);
-                    event.accept(ModContent.TIN_INGOT);
-                    event.accept(ModContent.STEEL_BLOCK);
-                    event.accept(ModContent.BRONZE_BLOCK);
-                    event.accept(ModContent.LEAD_BLOCK);
-                    event.accept(ModContent.SILVER_BLOCK);
-                    event.accept(ModContent.TIN_BLOCK);
-                    event.accept(ModContent.STEEL_NUGGET);
-                    event.accept(ModContent.BRONZE_NUGGET);
-                    event.accept(ModContent.LEAD_NUGGET);
-                    event.accept(ModContent.SILVER_NUGGET);
-                    event.accept(ModContent.TIN_NUGGET);
-                    event.accept(ModContent.COPPER_NUGGET);
-                    event.accept(ModContent.STEEL_SWORD);
-                    event.accept(ModContent.STEEL_PICKAXE);
-                    event.accept(ModContent.STEEL_AXE);
-                    event.accept(ModContent.STEEL_SHOVEL);
-                    event.accept(ModContent.STEEL_HOE);
-                    event.accept(ModContent.BRONZE_SWORD);
-                    event.accept(ModContent.BRONZE_PICKAXE);
-                    event.accept(ModContent.BRONZE_AXE);
-                    event.accept(ModContent.BRONZE_SHOVEL);
-                    event.accept(ModContent.BRONZE_HOE);
-                    event.accept(ModContent.STEEL_HELMET);
-                    event.accept(ModContent.STEEL_CHESTPLATE);
-                    event.accept(ModContent.STEEL_LEGGINGS);
-                    event.accept(ModContent.STEEL_BOOTS);
-                    event.accept(ModContent.BRONZE_HELMET);
-                    event.accept(ModContent.BRONZE_CHESTPLATE);
-                    event.accept(ModContent.BRONZE_LEGGINGS);
-                    event.accept(ModContent.BRONZE_BOOTS);
-                    event.accept(ModContent.STONE_PICK_HAMMER);
-                    event.accept(ModContent.IRON_PICK_HAMMER);
-                    event.accept(ModContent.GOLDEN_PICK_HAMMER);
-                    event.accept(ModContent.DIAMOND_PICK_HAMMER);
-                    event.accept(ModContent.NETHERITE_PICK_HAMMER);
-                    event.accept(ModContent.ENDERITE_PICK_HAMMER);
-                    event.accept(ModContent.STEEL_PICK_HAMMER);
-                    event.accept(ModContent.BRONZE_PICK_HAMMER);
+                    for (Item item : ModContent.ITEMS)
+                        event.accept(item);
                 })
                 .title(Component.translatable("itemGroup.ingotcraft.tab_ingotcraft"))
                 .build();
--- forge/src/main/java/com/jahirtrap/ingotcraft/init/ModTab.java
+++ forge/src/main/java/com/jahirtrap/ingotcraft/init/ModTab.java
@@ -3,68 +3,20 @@ package com.jahirtrap.ingotcraft.init;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
 
 import static com.jahirtrap.ingotcraft.IngotcraftMod.MODID;
 
 public class ModTab {
-    public static void register(CreativeModeTabEvent.Register register) {
+    private static void register(CreativeModeTabEvent.Register register) {
         CreativeModeTab TAB_INGOTCRAFT = register.registerCreativeModeTab(new ResourceLocation(MODID, "tab_ingotcraft"), builder -> builder.icon(() -> new ItemStack(ModContent.NETHERITE_PICK_HAMMER.get()))
                 .displayItems((features, event) -> {
-                    event.accept(ModContent.RAW_STEEL.get());
-                    event.accept(ModContent.RAW_BRONZE.get());
-                    event.accept(ModContent.RAW_LEAD.get());
-                    event.accept(ModContent.RAW_SILVER.get());
-                    event.accept(ModContent.RAW_TIN.get());
-                    event.accept(ModContent.RAW_STEEL_BLOCK.get());
-                    event.accept(ModContent.RAW_BRONZE_BLOCK.get());
-                    event.accept(ModContent.RAW_LEAD_BLOCK.get());
-                    event.accept(ModContent.RAW_SILVER_BLOCK.get());
-                    event.accept(ModContent.RAW_TIN_BLOCK.get());
-                    event.accept(ModContent.STEEL_INGOT.get());
-                    event.accept(ModContent.BRONZE_INGOT.get());
-                    event.accept(ModContent.LEAD_INGOT.get());
-                    event.accept(ModContent.SILVER_INGOT.get());
-                    event.accept(ModContent.TIN_INGOT.get());
-                    event.accept(ModContent.STEEL_BLOCK.get());
-                    event.accept(ModContent.BRONZE_BLOCK.get());
-                    event.accept(ModContent.LEAD_BLOCK.get());
-                    event.accept(ModContent.SILVER_BLOCK.get());
-                    event.accept(ModContent.TIN_BLOCK.get());
-                    event.accept(ModContent.STEEL_NUGGET.get());
-                    event.accept(ModContent.BRONZE_NUGGET.get());
-                    event.accept(ModContent.LEAD_NUGGET.get());
-                    event.accept(ModContent.SILVER_NUGGET.get());
-                    event.accept(ModContent.TIN_NUGGET.get());
-                    event.accept(ModContent.COPPER_NUGGET.get());
-                    event.accept(ModContent.STEEL_SWORD.get());
-                    event.accept(ModContent.STEEL_PICKAXE.get());
-                    event.accept(ModContent.STEEL_AXE.get());
-                    event.accept(ModContent.STEEL_SHOVEL.get());
-                    event.accept(ModContent.STEEL_HOE.get());
-                    event.accept(ModContent.BRONZE_SWORD.get());
-                    event.accept(ModContent.BRONZE_PICKAXE.get());
-                    event.accept(ModContent.BRONZE_AXE.get());
-                    event.accept(ModContent.BRONZE_SHOVEL.get());
-                    event.accept(ModContent.BRONZE_HOE.get());
-                    event.accept(ModContent.STEEL_HELMET.get());
-                    event.accept(ModContent.STEEL_CHESTPLATE.get());
-                    event.accept(ModContent.STEEL_LEGGINGS.get());
-                    event.accept(ModContent.STEEL_BOOTS.get());
-                    event.accept(ModContent.BRONZE_HELMET.get());
-                    event.accept(ModContent.BRONZE_CHESTPLATE.get());
-                    event.accept(ModContent.BRONZE_LEGGINGS.get());
-                    event.accept(ModContent.BRONZE_BOOTS.get());
-                    event.accept(ModContent.STONE_PICK_HAMMER.get());
-                    event.accept(ModContent.IRON_PICK_HAMMER.get());
-                    event.accept(ModContent.GOLDEN_PICK_HAMMER.get());
-                    event.accept(ModContent.DIAMOND_PICK_HAMMER.get());
-                    event.accept(ModContent.NETHERITE_PICK_HAMMER.get());
-                    event.accept(ModContent.ENDERITE_PICK_HAMMER.get());
-                    event.accept(ModContent.STEEL_PICK_HAMMER.get());
-                    event.accept(ModContent.BRONZE_PICK_HAMMER.get());
+                    for (RegistryObject<Item> item : ModContent.ITEMS.getEntries())
+                        event.accept(item.get());
                 })
                 .title(Component.translatable("itemGroup.ingotcraft.tab_ingotcraft"))
                 .build());
