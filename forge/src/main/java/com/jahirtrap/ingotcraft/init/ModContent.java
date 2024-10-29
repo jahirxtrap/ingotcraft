4b48196a39e842e9421952729f755a46d1e35f16
jahirxtrap <jahirroman2002@gmail.com>
2024-10-28 21:21:19 -0500

    Update environment, internal changes

--- forge/src/main/java/com/jahirtrap/ingotcraft/init/ModContent.java
+++ forge/src/main/java/com/jahirtrap/ingotcraft/init/ModContent.java
@@ -1,6 +1,5 @@
 package com.jahirtrap.ingotcraft.init;
 
-import com.jahirtrap.ingotcraft.block.BaseBlock;
 import com.jahirtrap.ingotcraft.item.BaseArmorItem;
 import com.jahirtrap.ingotcraft.item.BasePickHammerItem;
 import net.minecraft.core.registries.Registries;
@@ -9,6 +8,7 @@ import net.minecraft.world.item.ArmorItem.Type;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -28,21 +28,21 @@ public class ModContent {
     public static final RegistryObject<Item> RAW_LEAD = registerItem("raw_lead", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_SILVER = registerItem("raw_silver", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_TIN = registerItem("raw_tin", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Block> RAW_STEEL_BLOCK = registerBlock("raw_steel_block", () -> new BaseBlock(MapColor.STONE, SoundType.STONE), new Item.Properties());
-    public static final RegistryObject<Block> RAW_BRONZE_BLOCK = registerBlock("raw_bronze_block", () -> new BaseBlock(MapColor.STONE, SoundType.STONE), new Item.Properties());
-    public static final RegistryObject<Block> RAW_LEAD_BLOCK = registerBlock("raw_lead_block", () -> new BaseBlock(MapColor.STONE, SoundType.STONE), new Item.Properties());
-    public static final RegistryObject<Block> RAW_SILVER_BLOCK = registerBlock("raw_silver_block", () -> new BaseBlock(MapColor.STONE, SoundType.STONE), new Item.Properties());
-    public static final RegistryObject<Block> RAW_TIN_BLOCK = registerBlock("raw_tin_block", () -> new BaseBlock(MapColor.STONE, SoundType.STONE), new Item.Properties());
+    public static final RegistryObject<Block> RAW_STEEL_BLOCK = registerBlock("raw_steel_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GRAY).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final RegistryObject<Block> RAW_BRONZE_BLOCK = registerBlock("raw_bronze_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_ORANGE).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final RegistryObject<Block> RAW_LEAD_BLOCK = registerBlock("raw_lead_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLUE).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final RegistryObject<Block> RAW_SILVER_BLOCK = registerBlock("raw_silver_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_LIGHT_GRAY).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final RegistryObject<Block> RAW_TIN_BLOCK = registerBlock("raw_tin_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PINK).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
     public static final RegistryObject<Item> STEEL_INGOT = registerItem("steel_ingot", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BRONZE_INGOT = registerItem("bronze_ingot", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> LEAD_INGOT = registerItem("lead_ingot", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> SILVER_INGOT = registerItem("silver_ingot", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> TIN_INGOT = registerItem("tin_ingot", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Block> STEEL_BLOCK = registerBlock("steel_block", () -> new BaseBlock(MapColor.METAL, SoundType.METAL), new Item.Properties());
-    public static final RegistryObject<Block> BRONZE_BLOCK = registerBlock("bronze_block", () -> new BaseBlock(MapColor.METAL, SoundType.METAL), new Item.Properties());
-    public static final RegistryObject<Block> LEAD_BLOCK = registerBlock("lead_block", () -> new BaseBlock(MapColor.METAL, SoundType.COPPER), new Item.Properties());
-    public static final RegistryObject<Block> SILVER_BLOCK = registerBlock("silver_block", () -> new BaseBlock(MapColor.METAL, SoundType.METAL), new Item.Properties());
-    public static final RegistryObject<Block> TIN_BLOCK = registerBlock("tin_block", () -> new BaseBlock(MapColor.METAL, SoundType.METAL), new Item.Properties());
+    public static final RegistryObject<Block> STEEL_BLOCK = registerBlock("steel_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GRAY).sound(SoundType.METAL).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final RegistryObject<Block> BRONZE_BLOCK = registerBlock("bronze_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_ORANGE).sound(SoundType.METAL).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final RegistryObject<Block> LEAD_BLOCK = registerBlock("lead_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLUE).sound(SoundType.COPPER).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final RegistryObject<Block> SILVER_BLOCK = registerBlock("silver_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_LIGHT_GRAY).sound(SoundType.METAL).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final RegistryObject<Block> TIN_BLOCK = registerBlock("tin_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PINK).sound(SoundType.METAL).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
     public static final RegistryObject<Item> STEEL_NUGGET = registerItem("steel_nugget", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BRONZE_NUGGET = registerItem("bronze_nugget", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> LEAD_NUGGET = registerItem("lead_nugget", () -> new Item(new Item.Properties()));
@@ -62,9 +62,9 @@ public class ModContent {
     public static final RegistryObject<Item> STEEL_PICK_HAMMER = registerItem("steel_pick_hammer", () -> new BasePickHammerItem(ModTiers.STEEL_PICK_HAMMER, new Item.Properties()));
     public static final RegistryObject<Item> BRONZE_PICK_HAMMER = registerItem("bronze_pick_hammer", () -> new BasePickHammerItem(ModTiers.BRONZE_PICK_HAMMER, new Item.Properties()));
 
-    public static RegistryObject<Block> registerBlock(String name, Supplier<Block> supplier, Item.Properties properties) {
+    public static RegistryObject<Block> registerBlock(String name, Supplier<Block> supplier, Item.Properties itemProp) {
         var blockReg = registerBlock(name, supplier);
-        registerItem(name, () -> new BlockItem(blockReg.get(), properties));
+        registerItem(name, () -> new BlockItem(blockReg.get(), itemProp));
         return blockReg;
     }
 
@@ -76,23 +76,23 @@ public class ModContent {
         return ITEMS.register(name, supplier);
     }
 
-    private static List<RegistryObject<Item>> registerTools(String name, Tier tier, float[] attr, Item.Properties properties) {
+    private static List<RegistryObject<Item>> registerTools(String name, Tier tier, float[] attr, Item.Properties itemProp) {
         return List.of(
-                registerItem(name + "_sword", () -> new SwordItem(tier, 3, -2.4f, properties)),
-                registerItem(name + "_pickaxe", () -> new PickaxeItem(tier, 1, -2.8f, properties)),
-                registerItem(name + "_axe", () -> new AxeItem(tier, attr[0], attr[1], properties)),
-                registerItem(name + "_shovel", () -> new ShovelItem(tier, 1.5f, -3f, properties)),
-                registerItem(name + "_hoe", () -> new HoeItem(tier, (int) attr[2], attr[3], properties))
+                registerItem(name + "_sword", () -> new SwordItem(tier, 3, -2.4f, itemProp)),
+                registerItem(name + "_pickaxe", () -> new PickaxeItem(tier, 1, -2.8f, itemProp)),
+                registerItem(name + "_axe", () -> new AxeItem(tier, attr[0], attr[1], itemProp)),
+                registerItem(name + "_shovel", () -> new ShovelItem(tier, 1.5f, -3f, itemProp)),
+                registerItem(name + "_hoe", () -> new HoeItem(tier, (int) attr[2], attr[3], itemProp))
         );
     }
 
-    private static List<RegistryObject<Item>> registerArmor(ArmorMaterial material, Item.Properties properties) {
+    private static List<RegistryObject<Item>> registerArmor(ArmorMaterial material, Item.Properties itemProp) {
         String name = material.getName().substring(material.getName().indexOf(ResourceLocation.NAMESPACE_SEPARATOR) + 1);
         return List.of(
-                registerItem(name + "_helmet", () -> new BaseArmorItem(material, Type.HELMET, properties)),
-                registerItem(name + "_chestplate", () -> new BaseArmorItem(material, Type.CHESTPLATE, properties)),
-                registerItem(name + "_leggings", () -> new BaseArmorItem(material, Type.LEGGINGS, properties)),
-                registerItem(name + "_boots", () -> new BaseArmorItem(material, Type.BOOTS, properties))
+                registerItem(name + "_helmet", () -> new BaseArmorItem(material, Type.HELMET, itemProp)),
+                registerItem(name + "_chestplate", () -> new BaseArmorItem(material, Type.CHESTPLATE, itemProp)),
+                registerItem(name + "_leggings", () -> new BaseArmorItem(material, Type.LEGGINGS, itemProp)),
+                registerItem(name + "_boots", () -> new BaseArmorItem(material, Type.BOOTS, itemProp))
         );
     }
 
