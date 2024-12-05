f23bf4848b29544e02a335362a429f2e2fb2cd26
jahirxtrap <jahirroman2002@gmail.com>
2024-12-04 20:28:16 -0500

    1.21.4

--- fabric/src/main/java/com/jahirtrap/ingotcraft/init/ModMaterials.java
+++ fabric/src/main/java/com/jahirtrap/ingotcraft/init/ModMaterials.java
@@ -1,11 +1,14 @@
 package com.jahirtrap.ingotcraft.init;
 
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorType;
+import net.minecraft.world.item.equipment.EquipmentAsset;
+import net.minecraft.world.item.equipment.EquipmentAssets;
 
 import java.util.EnumMap;
 
@@ -14,14 +17,18 @@ import static com.jahirtrap.ingotcraft.IngotcraftMod.MODID;
 public class ModMaterials {
     public interface Armor {
         ArmorMaterial STEEL = new ArmorMaterial(25, createMap(new int[]{2, 5, 6, 2, 5}),
-                9, SoundEvents.ARMOR_EQUIP_IRON, 1f, 0f, ModTags.Items.STEEL_INGOTS, ResourceLocation.fromNamespaceAndPath(MODID, "steel"));
+                9, SoundEvents.ARMOR_EQUIP_IRON, 1f, 0f, ModTags.Items.STEEL_INGOTS, createAsset("steel"));
         ArmorMaterial BRONZE = new ArmorMaterial(15, createMap(new int[]{2, 5, 6, 2, 5}),
-                16, SoundEvents.ARMOR_EQUIP_GOLD, 0f, 0f, ModTags.Items.BRONZE_INGOTS, ResourceLocation.fromNamespaceAndPath(MODID, "bronze"));
+                16, SoundEvents.ARMOR_EQUIP_GOLD, 0f, 0f, ModTags.Items.BRONZE_INGOTS, createAsset("bronze"));
 
         private static EnumMap<ArmorType, Integer> createMap(int[] values) {
             EnumMap<ArmorType, Integer> enumMap = new EnumMap<>(ArmorType.class);
             for (int i = 0; i < values.length; i++) enumMap.put(ArmorType.values()[i], values[i]);
             return enumMap;
+        }
+
+        private static ResourceKey<EquipmentAsset> createAsset(String name) {
+            return ResourceKey.create(EquipmentAssets.ROOT_ID, ResourceLocation.fromNamespaceAndPath(MODID, name));
         }
     }
 
