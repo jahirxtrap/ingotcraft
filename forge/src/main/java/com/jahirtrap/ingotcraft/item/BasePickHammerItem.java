0bf34c9182f82577cc7a18f75f1f222eb867c904
jahirxtrap <jahirroman2002@gmail.com>
2025-03-27 15:17:30 -0500

    Forge 1.21.5

--- forge/src/main/java/com/jahirtrap/ingotcraft/item/BasePickHammerItem.java
+++ forge/src/main/java/com/jahirtrap/ingotcraft/item/BasePickHammerItem.java
@@ -1,15 +1,15 @@
 package com.jahirtrap.ingotcraft.item;
 
 import com.jahirtrap.ingotcraft.util.RepairableItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.ToolMaterial;
 
 import static com.jahirtrap.ingotcraft.util.CommonUtils.hurt;
 
-public class BasePickHammerItem extends PickaxeItem implements RepairableItem {
+public class BasePickHammerItem extends Item implements RepairableItem {
     public BasePickHammerItem(ToolMaterial material, Properties properties) {
-        super(material, 7f, -3f, properties);
+        super(properties.pickaxe(material, 7f, -3f));
     }
 
     @Override
