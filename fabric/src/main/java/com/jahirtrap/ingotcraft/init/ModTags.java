05aa8d5432e3d915a2783a1518832dbadfecb5c0
jahirxtrap <jahirroman2002@gmail.com>
2025-12-24 01:12:44 -0500

    1.21.11

--- fabric/src/main/java/com/jahirtrap/ingotcraft/init/ModTags.java
+++ fabric/src/main/java/com/jahirtrap/ingotcraft/init/ModTags.java
@@ -1,18 +1,18 @@
 package com.jahirtrap.ingotcraft.init;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 public class ModTags {
     public interface Items {
-        TagKey<Item> STEEL_INGOTS = create(ResourceLocation.parse("c:ingots/steel"));
-        TagKey<Item> BRONZE_INGOTS = create(ResourceLocation.parse("c:ingots/bronze"));
-        TagKey<Item> ZURITE_INGOTS = create(ResourceLocation.parse("c:ingots/zurite"));
-        TagKey<Item> ENDERITE_INGOTS = create(ResourceLocation.parse("c:ingots/enderite"));
+        TagKey<Item> STEEL_INGOTS = create(Identifier.parse("c:ingots/steel"));
+        TagKey<Item> BRONZE_INGOTS = create(Identifier.parse("c:ingots/bronze"));
+        TagKey<Item> ZURITE_INGOTS = create(Identifier.parse("c:ingots/zurite"));
+        TagKey<Item> ENDERITE_INGOTS = create(Identifier.parse("c:ingots/enderite"));
 
-        private static TagKey<Item> create(ResourceLocation name) {
+        private static TagKey<Item> create(Identifier name) {
             return TagKey.create(Registries.ITEM, name);
         }
     }
